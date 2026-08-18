7a79a3551811c7a10102678c00bd0626b6010881
64294 <642944843@qq.com>
2026-08-18 13:49:35 +0800

    114514

--- src/FusionPlanet/ExampleJavaMod.java
+++ src/FusionPlanet/ExampleJavaMod.java
@@ -2,9 +2,11 @@ package FusionPlanet;
 
 import FusionPlanet.content.fPlanets;
 import arc.*;
+import arc.struct.Seq;
 import arc.util.*;
 import mindustry.game.EventType.*;
 import mindustry.mod.*;
+import mindustry.type.Planet;
 import mindustry.ui.dialogs.*;
 import mindustry.ctype.UnlockableContent;
 import mindustry.Vars;
@@ -16,8 +18,8 @@ public class ExampleJavaMod extends Mod{
 
         Events.on(ClientLoadEvent.class, e -> {
             Time.runTask(10f, () -> {
-                BaseDialog dialog = new BaseDialog("雷霆大青蛙");
-                dialog.cont.add("behold").row();
+                BaseDialog dialog = new BaseDialog("frog");
+                dialog.cont.add("雷霆大青蛙").row();
                 dialog.cont.image(Core.atlas.find("example-java-mod-frog")).pad(20f).row();
                 dialog.cont.button("I am blind", dialog::hide).size(200f, 50f);
                 dialog.show();
@@ -28,10 +30,17 @@ public class ExampleJavaMod extends Mod{
     @Override
     public void loadContent(){
         Log.info("Loading some example content.");
-
+        fPlanets.load();
+        Planet fusion = Vars.content.planets().find(p -> p.name.equals("fusion-planet"));
+        if (fusion == null) {
+            Log.err("Fusion planet not found in content!");
+            return;
+        }
+        Log.info("Fusion Planet loaded!");
         Events.on(ContentInitEvent.class, e -> {
             for (UnlockableContent c : Vars.content.blocks()) {
                 if (c.minfo.mod == null) c.alwaysUnlocked = true;
+                c.shownPlanets = Seq.with(fusion).asSet();
             }
             for (UnlockableContent c : Vars.content.items()) {
                 if (c.minfo.mod == null) c.alwaysUnlocked = true;
@@ -48,8 +57,7 @@ public class ExampleJavaMod extends Mod{
             Log.info("All vanilla content unlocked!");
         });
 
-        fPlanets.load();
-        Log.info("Fusion Planet loaded!");
+
     }
 
 }
