5e2dc49b19b13604ab61dd4a26243535ed299c55
64294 <642944843@qq.com>
2026-08-19 15:17:55 +0800

    114514

--- src/FusionPlanet/content/fPlanets.java
+++ src/FusionPlanet/content/fPlanets.java
@@ -2,14 +2,13 @@ package FusionPlanet.content;
 
 import arc.graphics.Color;
 import arc.math.Mathf;
+import arc.math.geom.Mat3D;
 import arc.math.geom.Vec3;
 import mindustry.Vars;
 import mindustry.content.Blocks;
 import mindustry.content.Planets;
 import mindustry.game.Team;
-import mindustry.graphics.g3d.HexMesh;
-import mindustry.graphics.g3d.HexSkyMesh;
-import mindustry.graphics.g3d.MultiMesh;
+import mindustry.graphics.g3d.*;
 import mindustry.maps.generators.PlanetGenerator;
 import mindustry.type.Planet;
 import mindustry.world.TileGen;
@@ -56,23 +55,57 @@ public class fPlanets {
 
         ringWorld = new Planet("ring-world", fusionPlanet, 1.2f, 2) {{
             generator = new RingPlanetGenerator();
-            meshLoader = () -> new HexMesh(this, 6);
+
+            meshLoader = () -> {
+                int segments = 24;
+                float ringRadius = 0.9f;
+                float ringWidth = 0.25f;
+                float ringHeight = 0.04f;
+
+                Color metalColor = Color.valueOf("555555");
+                Color tintColor = Color.valueOf("333333");
+
+                GenericMesh[] meshes = new GenericMesh[segments];
+
+                for (int i = 0; i < segments; i++) {
+                    float angle = (i / (float) segments) * 360f;
+
+                    NoiseMesh segment = new NoiseMesh(
+                            ringWorld, i + 100, 1,
+                            ringWidth,
+                            2, 0.5f, 0.4f, 12f,
+                            metalColor, tintColor,
+                            2, 0.5f, 0.4f, 0.3f
+                    );
+                    Mat3D mat = new Mat3D();
+                    mat.setToTranslation(new Vec3(0, 0, ringRadius));
+                    mat.rotate(0, 1, 0, angle);
+                    Mat3D tilt = new Mat3D();
+                    tilt.rotate(1, 0, 0, 30);
+                    mat.mul(tilt);
+                    Mat3D flatten = new Mat3D();
+                    flatten.scale(1f, ringHeight / ringWidth, 1f);
+                    mat.mul(flatten);
+
+                    meshes[i] = new MatMesh(segment, mat);
+                }
+
+                return new MultiMesh(meshes);
+            };
+
             localizedName = "Ring World";
             visible = true;
             accessible = true;
             alwaysUnlocked = true;
             bloom = false;
             defaultEnv = Env.terrestrial;
-            atmosphereColor = Color.valueOf("886644");
-            atmosphereRadIn = 0.01f;
-            atmosphereRadOut = 0.2f;
             allowLaunchToNumbered = true;
             startSector = 0;
             defaultCore = Blocks.coreShard;
-            // 轨道：紧贴母星，视觉上呈现环状
             orbitRadius = 0.001f;
             orbitTime = 60 * 60;
             rotateTime = 30 * 60;
+
             ruleSetter = r -> {
                 r.waveTeam = Team.crux;
                 r.waves = true;
